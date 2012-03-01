dc264607345abdad5da665ef1a57d339593fcb17
Gökmen Göksel <gokmeng@gmail.com>
2012-03-02 01:48:48 +0200

    Add fancybox plug-in

---
+++ plugins/fancybox/plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+    # Copyright (C) 2012, Nuf (~Fun)
+    # 2011 - Gökmen Göksel <gokmen@goksel.me>
+    #        http://github.com/gokmen
+
+    # This program is free software; you can redistribute it and/or modify it
+    # under the terms of the GNU General Public License as published by the Free
+    # Software Foundation; either version 2 of the License, or (at your option)
+    # any later version.
+
+    $_template = dirname(__FILE__)."/fancybox-header.html";
+    $_plugin_header .= readContent($_template);
+
+?>
